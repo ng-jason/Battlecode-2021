04b33d0cd6bb7fcc845b6b7c6d488c96512da3d9
ng-jason <nghyphenjason@gmail.com>
2021-01-12 08:22:30 +1000

    Splitted up code from player folder
    using code from previous Battlecodes

---
+++ src/sprint/Politician.java
@@ -0,0 +1,30 @@
+package sprint;
+
+import battlecode.common.*;
+
+public class Politician extends Robot {
+
+    static final double polPassFactor = 1;
+
+
+    public Politician(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        Team enemy = rc.getTeam().opponent();
+        int actionRadius = rc.getType().actionRadiusSquared;
+        if (Math.random() < polPassFactor * rc.sensePassability(rc.getLocation())) {
+            if (rc.senseNearbyRobots().length * 2 >= rc.getConviction()
+                    && rc.canEmpower(actionRadius)) {
+                System.out.println("empowering...");
+                rc.empower(actionRadius);
+                System.out.println("empowered");
+                return;
+            }
+        }
+        if (tryMove(randomDirection()))
+            System.out.println("I moved!");
+    }
+}
