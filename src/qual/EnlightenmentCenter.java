c7a5ba4187cf93a0246bf56b12294f2eab78ece6
ng-jason <nghyphenjason@gmail.com>
2021-01-24 22:49:47 +1000

    Newly spawned robots will now move start exploring if there is no flag set by EC (flag == 0)

--- src/qual/EnlightenmentCenter.java
+++ src/qual/EnlightenmentCenter.java
@@ -12,10 +12,14 @@ public class EnlightenmentCenter extends Robot {
     static int robotCount = 0;
     static int[] robotIDs; // IDs of robot can use to see if they are still alive etc using senseID
     static MapLocation[] enlightenmentCenterLocations;
+    static Direction currentSpawnDirection = Direction.NORTH;
 
 
     public EnlightenmentCenter(RobotController rc) throws GameActionException {
         super(rc);
+        if (rc.getRoundNum() < 1) {
+            rc.setFlag(0);
+        }
     }
 
     @Override
@@ -26,31 +30,27 @@ public class EnlightenmentCenter extends Robot {
 //        if (target != null) {
 //            Direction d = rc.getLocation().directionTo(target);
 //        }
-        for (Direction dir : directions) {
-            if (rc.canBuildRobot(toBuild, dir, influence)) {
-                rc.buildRobot(toBuild, dir, influence);
-                // add robot IDs to list, not working atm
+        System.out.println("Current spawn dir" + currentSpawnDirection.toString());
+        if (rc.canBuildRobot(toBuild, currentSpawnDirection, influence)) {
+            rc.buildRobot(toBuild, currentSpawnDirection, influence);
+            // add robot IDs to list, not working atm
 //                for (RobotInfo robot : rc.senseNearbyRobots(1, rc.getTeam())) {
 //                    if (rc.adjacentLocation(dir) == robot.getLocation()) {
 //                        robotIDs[robotCount++] = robot.getID();
 //                    }
 //                }
-            } else {
-                break;
-            }
         }
-        for (int robotID : robotIDs) {
-            System.out.println(robotID);
-        }
-        if (rc.isReady()) {
-            if (target == null) {
-                // get location flag from robot if any (but need robot ID)
-                //
-//                MapLocation target =
-            }
+        currentSpawnDirection = currentSpawnDirection.rotateRight();
+//        for (int robotID : robotIDs) {
+//            System.out.println(robotID);
+//        }
+        if (target != null) {
             sendLocation(target);
-            // sendLocation(target, 1);  // for sending extra info like target type
+        } else {
+            // get a location target from a random friendly robot
+
         }
+        // sendLocation(target, 1);  // for sending extra info like target type
     }
 
     /**
